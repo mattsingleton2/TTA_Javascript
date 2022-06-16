3189f71b6a8f108a04b8e108f57eb802091f3f43
Matthew Singleton <matt.singleton2@gmail.com>
2022-06-16 08:41:53 -0500

    Javascript practice

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,23 @@
+window.alert("Hello, World");
+
+document.write('Hello, world...');
+
+var A = "\nThis is a string.";
+
+document.write(A);
+
+var B = "\nThis is a string" + " for a window alert.";
+
+window.alert(B);
+
+var C = "\nHe sighed, mumbling \"You\'ve got to be kidding me.\"";
+
+document.write(C);
+
+document.write(A + ' ' + B + ' ' + C);
+
+var name = "Matt", age = 33, height = "5\'7\"";
+
+document.write(name + ' ' + age + ' ' + height);
+
+var result = 6 + 6;
