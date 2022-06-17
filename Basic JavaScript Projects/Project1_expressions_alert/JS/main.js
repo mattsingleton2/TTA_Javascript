2e58d777865f9d92b3b7a2c4efc279e440647bef
Matthew Singleton <matt.singleton2@gmail.com>
2022-06-16 20:31:36 -0500

    Updated js files

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,16 @@
+
+function wheelResize(event) {
+    var y = event.deltaY;                           //establishing the y variable to grab how MUCH the mousewheel scrolled on a Y-axis.
+    var currentSize = event.target.style.width;     //establishing the currentSize variable to grab the current size of the targeted event element. Specifically width.
+    if (y > 0) {                                    //If we went positive with the scroll size...
+        newSize = parseInt(currentSize) + 10;       //Then parse currentSize as an integer and add 10 to it (we'll add the px definition momentarily)
+    } else {                                        //Similarly, if we went negative with the scroll size...
+        newSize = parseInt(currentSize) - 10;       //Then let's remove 10 from it. (px definition coming momentarily)
+    }
+    event.target.style.width = newSize + "px";      //We've got our newSize so we need to adjust the STYLE of the event target and change the width with the new size in px.
+    event.target.style.height = newSize + "px";     //Same thing, but for height.
+}
+
 window.alert("Hello, World"); //Pops open a window alert
 
 document.write('Hello, world...'); //writes some text
@@ -21,3 +34,4 @@ var name = "Matt", age = 33, height = "5\'7\""; //Multi-variable instantiation o
 document.write(name + ' ' + age + ' ' + height); //Concatenating those and writing them.
 
 var result = 6 + 6; //Instantiates variable 'result' and stores the value of an operation within it.
+
