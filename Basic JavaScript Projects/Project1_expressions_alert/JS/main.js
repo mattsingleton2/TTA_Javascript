e87140d17b2c379a24cbaf9387c2fea7c641ed75
Matthew Singleton <matt.singleton2@gmail.com>
2022-06-16 19:53:48 -0500

    Update main.js

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,23 +1,23 @@
-window.alert("Hello, World");
+window.alert("Hello, World"); //Pops open a window alert
 
-document.write('Hello, world...');
+document.write('Hello, world...'); //writes some text
+ 
+var A = "\nThis is a string."; //Instantiates a variable 'A' and assigns it a string.
 
-var A = "\nThis is a string.";
+document.write(A); //Writes that variable on the document.
 
-document.write(A);
+var B = "\nThis is a string" + " for a window alert."; //Concatenating strings and storing them in the instantiated variable, "B"
 
-var B = "\nThis is a string" + " for a window alert.";
+window.alert(B); //Creates an alert calling the variable B
 
-window.alert(B);
+var C = "\nHe sighed, mumbling \"You\'ve got to be kidding me.\""; //More concatenation practice with escape characters.
 
-var C = "\nHe sighed, mumbling \"You\'ve got to be kidding me.\"";
+document.write(C); //Just showing that it works...
 
-document.write(C);
+document.write(A + ' ' + B + ' ' + C); //Conca + tenation
 
-document.write(A + ' ' + B + ' ' + C);
+var name = "Matt", age = 33, height = "5\'7\""; //Multi-variable instantiation on a single line.
 
-var name = "Matt", age = 33, height = "5\'7\"";
+document.write(name + ' ' + age + ' ' + height); //Concatenating those and writing them.
 
-document.write(name + ' ' + age + ' ' + height);
-
-var result = 6 + 6;
+var result = 6 + 6; //Instantiates variable 'result' and stores the value of an operation within it.
