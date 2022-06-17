5040be9597756c142301aec0b1bfcb3387653de1
Matthew Singleton <matt.singleton2@gmail.com>
2022-06-16 21:01:58 -0500

    Started Project2 : Functions

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,7 @@
+function buttonUpdate() {
+    var str = "This is the button text!";
+    var sentence = "I am learning";
+    sentence += " a lot about JavaScript. Just please don\'t make me learn Redux.";
+    document.getElementById("Button_Text").innerHTML = str;
+    document.getElementById("concatenation").innerHTML = sentence;
+}
